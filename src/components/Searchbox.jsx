a195c0e523e712ca29393d343875f14c25126804
sashilshrestha <sashilstha5@gmail.com>
2020-06-07 21:01:11 +0545

    Started Project

---
+++ src/components/Searchbox.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Searchbox = () => {
+  return (
+    <>
+      <form className=" ">
+        <label className="" htmlFor="Query">
+          Movie Name
+        </label>
+        <input type="text" className="input" />
+      </form>
+    </>
+  );
+};
+
+export default Searchbox;
